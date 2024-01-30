a54637672fb65292abffa637b7d4e33607576865
romario lima <romariolima1006@gmail.com>
2024-01-30 10:05:10 -0300

    update model tadabase > Category

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from contact.models import Contact
+from contact.models import Category
 # Register your models here.
 
 
@@ -9,8 +10,14 @@ class ContactAdimin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'phone')
     ordering = ('-id',)
     list_filter = ('created_date',)
-    search_fields = ('id', 'first_name', 'last_name')
+    search_fields = ('id', 'first_name', 'last_name',)
     list_per_page = 10
     list_max_show_all = 100
     # list_editable = 'first_name', 'last_name',
     list_display_links = ('first_name', 'id')
+
+
+@admin.register(Category)
+class CategoryAdimin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('-id',)
