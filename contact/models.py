953c34b0ef598d2f59e064826ee740aee5899ea4
romario lima <romariolima1006@gmail.com>
2024-01-30 11:03:42 -0300

    add contacts in database with faker

--- contact/models.py
+++ contact/models.py
@@ -24,7 +24,7 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
-    picture = models.ImageField(blank=True, upload_to='picture/%Y/%m/' )
+    picture = models.ImageField(blank=True, upload_to='picture/%Y/%m/')
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
